862d09ea1f5a3b81b8d5618b5d6f4b1a347977b2
Angela Venable <avenable11@ivytech.edu>
2026-06-16 17:41:23 -0400

    more examples and lab activity

--- 06_16_26/main.cpp
+++ 06_16_26/main.cpp
@@ -1,42 +1,26 @@
 #include <iostream>
-#include <string>
 #include <regex>
-#include <format>
-#include "date.h"
+#include <string>
+#include <iomanip>
+#include <set>
+#include <fstream>
+
+// Module 2 part a lab create a regular expression to divide a tab-separated string
 
 int main()
 {
-    std::regex dateRegex(R"((\d{4})(\/|-)(0?[1-9]|1[0-2])(\2)(3[01]|[12][0-9]|0?[1-9])\b)");
-    std::regex q(R"(^q)");
-    while (true)
+    std::regex commaStr(R"(((?:\w+ *)+(?=,|$)))"); //((?:\w+ *)+(?=,))|(?<=, )(\w+ *)+
+    std::ifstream in("data.txt");
+    while (!in.eof() && in.is_open())
     {
-        std::cout << "Enter a date (year/month/day) (q=quit): ";
-        std::string str;
-        if (!getline(std::cin, str))
+        std::string line;
+        std::getline(in, line);
+        const std::sregex_iterator end;
+        for (std::sregex_iterator iter(std::cbegin(line), std::cend(line), commaStr); iter != end; ++iter)
         {
-            break;
-        }
-        if (std::regex_search(str, q))
-        {
-            break;
-        }
-        std::smatch m;
-        if (std::regex_search(str, m, dateRegex))
-        {
-            try
-            {
-                date myDate(stoi(m[3]), stoi(m[5]), stoi(m[1]));
-                std::cout << std::format("Valid Date:  Year={}, month={}, day={}", myDate.getYear(), myDate.getMonth(), myDate.getDay()) << std::endl;
-            }
-            catch (std::invalid_argument e)
-            {
-                std::cerr << e.what() << '\n';
-            }
-        }
-        else
-        {
-            std::cout << " invalid date" << std::endl;
+            std::cout << (*iter)[0].str() << std::endl;
         }
     }
+
     return 0;
 }
