a27ecc331d8407720de1529b24b88936bacf9677
ab1gurl64-max <ab1.gurl64@gmail.com>
2026-06-20 14:53:25 -0500

    tab regex
    Added functionality to read from 'data.txt' and print matched tab-separated values.

--- 06_16_26/main.cpp
+++ 06_16_26/main.cpp
@@ -22,5 +22,19 @@ int main()
         }
     }
 
+    
+    std::regex tabStr(R"([^\t\n]+)\t([^\t\n]+)\t([^\t\n]+)\t([^\t\n]+)");
+    std::ifstream in("data.txt");
+    while (!in.eof() && in.is_open())
+    {
+        std::string line;
+        std::getline(in, line);
+        const std::sregex_iterator end;
+        for (std::sregex_iterator iter(std::cbegin(line), std::cend(line), commaStr); iter != end; ++iter)
+        {
+            std::cout << (*iter)[0].str() << std::endl;
+        }
+    }
+
     return 0;
 }
